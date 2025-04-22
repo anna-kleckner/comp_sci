1a8c9d110b9b2b3c0b9feef6b0c06d8229b04c62
Anna <annakleckner08@gmail.com>
2025-04-22 14:34:05 -0500

    Add files via upload

--- python/combo_menu.py
+++ python/combo_menu.py
@@ -46,7 +46,7 @@ def iteration_2():
         print(f"You ordered a {user_sandwich} sandwich")
         print(f"Your total is ${total_price}")
 
-#iteration_2()
+#iteration_2() 
 
 def iteration_3():
     total_price = 0
@@ -114,6 +114,8 @@ def iteration_3():
 
 def iteration_4():
     total_price = 0
+    user_order = []
+
     print("Welcome to my shop! \nWe have three types of sandwiches: Chicken ($2), Beef ($3), Tofu ($4)")
     
     sandwiches = {
@@ -121,8 +123,7 @@ def iteration_4():
         "beef": 3,
         "tofu": 4 }
     user_sandwich = str(input("Sandwich type: "))
-    user_sandwich = user_sandwich.lower()
-
+    user_order.append(user_sandwich)
 
     beverages = {
         "small": 1,
@@ -131,10 +132,10 @@ def iteration_4():
     user_beverage = str(input("\nWe also have beverages: small ($1), medium ($1.50), large ($2). Would you like a drink? (yes or no) "))
     if user_beverage == "yes":
         beverage = str(input("Beverage size: "))
-        total_price = total_price + (sandwiches[user_sandwich]) + (beverages[beverage])
+        user_order.append(beverage)
     elif user_beverage == "no":
         total_price = total_price + 0
-
+    
     fries = {
         "small": 1,
         "medium": 1.5,
@@ -143,35 +144,39 @@ def iteration_4():
     user_fry = str(input("\nWe also have fries: small ($1), medium ($1.50), large ($2). Would you like fries? (yes or no) "))
     if user_fry == "yes":
         fry = str(input("Fry size: "))
+        user_order.append(fry)
         if fry == "small":
             str(input("Would you like to mega-size your fries? (yes or no) "))
             if "yes":
-                fry = "mega-size" 
+                fry = "mega-size"
+                user_order[2] = 'large'
+                user_order.append('mega-size')
             if "no":
                 fry = "small"
+                user_order.append(fry)
     if user_fry == "no":
         total_price = total_price + 0
     
     ketchup = int(input("How many ketchup packets would you like? (0+) "))
     
     if user_beverage == "yes" and user_fry == "yes":
-            total_price = total_price + (sandwiches[user_sandwich]) + (beverages[beverage]) + (fries[fry]) + (0.5 * ketchup) - 1
-            print(f"\nYou ordered a {user_sandwich} sandwich, a {beverage} beverage, and a {fry} fry")
-            print(f"Your total is ${total_price}")
+        total_price = total_price + (sandwiches[user_sandwich]) + (beverages[beverage]) + (fries[fry]) + (0.5 * ketchup) - 1
+        print(f"\nYou ordered a {user_order[0]} sandwich, a {user_order[1]} beverage, and a {user_order[2]} fry")
+        print(f"Your total is ${total_price:.2f}")
 
     if user_beverage == "no" and user_fry == "yes":
         total_price = total_price + (sandwiches[user_sandwich]) + (fries[fry]) + (0.5 * ketchup)
-        print(f"\nYou ordered a {user_sandwich} sandwich and a {fry} fry")
-        print(f"Your total is ${total_price}")
+        print(f"\nYou ordered a {user_order[0]} sandwich and a {user_order[1]} fry")
+        print(f"Your total is ${total_price:.2f}")
 
     if user_beverage == "yes" and user_fry == "no":
         total_price = total_price + (sandwiches[user_sandwich]) + (beverages[beverage]) + (0.5 * ketchup)
-        print(f"\nYou ordered a {user_sandwich} sandwich and a {beverage} beverage")
-        print(f"Your total is ${total_price}")
+        print(f"\nYou ordered a {user_order[0]} sandwich and a {user_order[1]} beverage")
+        print(f"Your total is ${total_price:.2f}")
 
     if user_beverage == "no" and user_fry == "no":
         total_price = total_price + (sandwiches[user_sandwich]) + (0.5 * ketchup)
-        print(f"\nYou ordered a {user_sandwich} sandwich")
+        print(f"\nYou ordered a {user_order[0]} sandwich")
         print(f"Your total is ${total_price:.2f}")
 
 iteration_4()
---
+++ python/comp_sci_test.py
@@ -0,0 +1,22 @@
+a = 1
+while a<6:
+  print(a)
+  a = a+1
+
+b = 10
+while b>0:
+  print(b)
+  b = b-1
+
+c = 9
+while c >= 1:
+  if c % 2 != 0:
+    print(c)
+  c -= 1
+
+d = 2
+while d <= 10:
+  if d % 2 == 0:
+    print(d)
+  d += 1
+
